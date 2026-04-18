fd0f3be8580e4ce7b2443f80761f8e364005557f
vishw <vishw@192.168.1.58>
2026-04-18 12:04:05 +0530

    Updated Operator code

--- src/com/java/core/Operators.java
+++ src/com/java/core/Operators.java
@@ -1,5 +1,33 @@
 package com.java.core;
 
 public class Operators {
+	public static void main(String[] args) {
+		
+		int a=25;
+		int b=25;
+	    int Sum=a+b;
+	 System.out.println(Sum);
+	    
+	    int c=90;
+	    int d=30;
+	    int Sub=c-d;
+	 System.out.println(Sub);
+	 
+	 int e=35;
+	 int f=2;
+	 int Mul=e*f;
+	System.out.println(Mul);
+	
+	 int g=160;
+	 int h=2;
+	 int Div=g/h;
+	 System.out.println(Div);
+	 
+	 int i=60;
+	 int j=120;
+	 int Mod=i%j;
+	 System.out.println(Mod);
+	 
+	}
 
 }
