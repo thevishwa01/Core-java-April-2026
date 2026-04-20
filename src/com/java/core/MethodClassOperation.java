ddc831612bd375af25c5e2632edcb04612cfb1f1
vishw <vishw@192.168.1.65>
2026-04-20 12:00:05 +0530

    Method Class Updated

---
+++ src/com/java/core/MethodClassOperation.java
@@ -0,0 +1,30 @@
+package com.java.core;
+
+public class MethodClassOperation {
+	public void Subtraction () {
+		int a =75;
+		int b=25;
+		int sub=a-b;
+	System.out.println("The Subtraction is "+sub);
+	}
+	
+public void Addition() {
+	int c=30;
+	int d=30;
+	int Add=c+d;
+	System.out.println("Addition is "+Add);
+}
+public void Multiplicatio() {
+	int e=35;
+	int f=2;
+	int Mul=e*f;
+	System.out.println("Multiplication is "+Mul);	
+}
+public void Division() {
+	int g=160;
+	int h=2;
+	int Div=g/h;
+	System.out.println("Division is "+Div);
+	
+}
+}
---
+++ src/com/java/core/TestMethod.java
@@ -0,0 +1,13 @@
+package com.java.core;
+
+public class TestMethod {
+ public static void main(String[] args) {
+	MethodClassOperation op=new MethodClassOperation();
+	op.Subtraction();
+	op.Addition();
+	op.Multiplicatio();
+	op.Division();
+	
+}
+
+}
