f59329deeeebea5c01ccad2fd7c2f1ff16b3e089
vishw <vishw@Vishwanath>
2026-04-22 13:14:05 +0530

    TowCalssVariable

---
+++ src/TwoClassVariable/Student.java
@@ -0,0 +1,9 @@
+package TwoClassVariable;
+
+public class Student {
+	
+	public int id;
+	public String name;
+	public String city;
+
+}
---
+++ src/TwoClassVariable/Test.java
@@ -0,0 +1,37 @@
+package TwoClassVariable;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		
+		Student s=new Student();
+		s.id= 101;
+		s.name="Vishwa";
+		s.city="Pune";
+		
+		Student s1=new Student();
+		s1.id=102;
+		s1.name="Sham";
+		s1.city="Mumbai";
+		
+	
+		System.out.println(s.id);
+		System.out.println(s.name);
+		System.out.println(s.city);
+		
+		System.out.println("");
+		
+		System.out.println(s1.id);
+		System.out.println(s1.name);
+		System.out.println(s1.city);
+		
+		
+	}
+
+
+	
+	
+	
+
+}
