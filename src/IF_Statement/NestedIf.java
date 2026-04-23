8cab69b7ce1459a4f85c3eeb2d9d7e4453c8dd50
vishw <vishw@Vishwanath>
2026-04-23 12:07:49 +0530

    Nested If

---
+++ src/IF_Statement/NestedIf.java
@@ -0,0 +1,16 @@
+package IF_Statement;
+
+public class NestedIf {
+
+	public static void main(String[] args) {
+		
+		double Percentage=75;
+		if (Percentage >=40) {
+			System.out.println("The Student is Passed ");
+		}
+	if (Percentage >=75) {
+		System.out.println("The Student is Passed with A-Grade");
+		
+	}
+	}
+	}
