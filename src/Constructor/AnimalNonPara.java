750b583a4bf70628c9a61e949741fabb920102ec
vishw <vishw@192.168.1.53>
2026-05-06 15:17:06 +0530

    Wrapper Class

--- src/Constructor/AnimalNonPara.java
+++ src/Constructor/AnimalNonPara.java
@@ -1,10 +1,11 @@
 package Constructor;
 
 public class AnimalNonPara {
-	
+
 	public AnimalNonPara() {
-		
+
 	}
+
 	public void printanimalname() {
 		System.out.println("Animal name is: Lion ");
 	}
--- src/Constructor/TestReturn.java
+++ src/Constructor/TestReturn.java
@@ -2,9 +2,9 @@ package Constructor;
 
 public class TestReturn {
 	public static void main(String[] args) {
-		
-		ConstructorReturn c=new ConstructorReturn();
-		String Animalname=c.printname();
+
+		ConstructorReturn c = new ConstructorReturn();
+		String Animalname = c.printname();
 		System.out.println(Animalname);
 	}
 
