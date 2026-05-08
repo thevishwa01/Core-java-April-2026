c6ab15c12fb4d8f7f44cf2f673d4774dffff50c9
Vishwanath <thevishwa01@gmail.com>
2026-05-08 14:25:03 +0530

    prefix

---
+++ Assignments/src/StringClassMethods/PreMethod.java
@@ -0,0 +1,12 @@
+package StringClassMethods;
+
+public class PreMethod {
+public static void main(String[] args) {
+	
+	String s="pre HelloWorld";
+	String s1="pre";
+	System.out.println(s.startsWith(s1));
+			
+}
+}
+//How would you check if a string starts with the prefix "pre" ?
