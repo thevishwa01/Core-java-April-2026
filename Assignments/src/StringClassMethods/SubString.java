dd7805d0c43cb892deb5f0a478f0e13b18f8a672
Vishwanath <thevishwa01@gmail.com>
2026-05-08 13:19:45 +0530

    SubString

---
+++ Assignments/src/StringClassMethods/SubString.java
@@ -0,0 +1,10 @@
+package StringClassMethods;
+
+public class SubString {
+public static void main(String[] args) {
+	
+	String s="Hello World";
+	System.out.println(s.substring(5));
+}
+}
+//Given String str = "HelloWorld";, what does str.substring(5) return?
