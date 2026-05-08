4cda255fe5bf42c690daebf9844cf4aef2f9e691
Vishwanath <thevishwa01@gmail.com>
2026-05-08 14:49:42 +0530

    Check contains

---
+++ Assignments/src/StringClassMethods/CheckSubstringsContains.java
@@ -0,0 +1,12 @@
+package StringClassMethods;
+
+public class CheckSubstringsContains {
+public static void main(String[] args) {
+	
+	String s="test one";
+	System.out.println(s.contains("test"));
+}
+}
+
+
+//How would you check if a string contains the substring "test" ?
