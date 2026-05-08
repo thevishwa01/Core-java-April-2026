5b7d2e698eb623833cebd61451b4e5ff18c415cd
Vishwanath <thevishwa01@gmail.com>
2026-05-08 13:31:15 +0530

    Comapre To

---
+++ Assignments/src/StringClassMethods/CompareTo.java
@@ -0,0 +1,14 @@
+package StringClassMethods;
+
+public class CompareTo {
+	public static void main(String[] args) {
+
+		String s = "Apple";
+		String s1 = "Banana";
+		System.out.println(s.compareTo(s1));
+	}
+}
+
+//compareTo compares the UNICODE/ASCII value of the character --------compareTo
+
+//What is the output of "apple".compareTo("banana") ? Explain why.
