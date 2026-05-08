042ed5a3b5ccab7de17fb623773f98a77e5e4315
Vishwanath <thevishwa01@gmail.com>
2026-05-08 14:44:24 +0530

    Contact

---
+++ Assignments/src/StringClassMethods/ContactMethod.java
@@ -0,0 +1,11 @@
+package StringClassMethods;
+
+public class ContactMethod {
+	public static void main(String[] args) {
+		
+		String s="Hello";
+		System.out.println(s.concat(" World"));
+	}
+
+}
+//What will "Hello".concat("World") return?
