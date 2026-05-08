4070030b94e8c1b720e1dfc8aaa2d18a6303630f
Vishwanath <thevishwa01@gmail.com>
2026-05-08 14:37:42 +0530

    Index of

---
+++ Assignments/src/StringClassMethods/IndexOf.java
@@ -0,0 +1,9 @@
+package StringClassMethods;
+
+public class IndexOf {
+public static void main(String[] args) {
+	
+	System.out.println("OpenAI".indexOf('A'));
+}
+}
+//What does "OpenAI".indexOf('A') return?
