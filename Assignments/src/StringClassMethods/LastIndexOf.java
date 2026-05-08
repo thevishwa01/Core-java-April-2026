343696974cafc9673c19d14b7cce21a1786c15f8
Vishwanath <thevishwa01@gmail.com>
2026-05-08 14:41:50 +0530

    Last Index Of

---
+++ Assignments/src/StringClassMethods/LastIndexOf.java
@@ -0,0 +1,11 @@
+package StringClassMethods;
+
+public class LastIndexOf {
+public static void main(String[] args) {
+	
+	System.out.println("abracadabra".lastIndexOf('a'));
+}
+}
+
+
+//Given String str = "abracadabra"; , what will str.lastIndexOf("a") return?
