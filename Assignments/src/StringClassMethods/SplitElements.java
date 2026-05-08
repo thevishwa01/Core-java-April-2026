c2468cdcb03f9c1fdecc74a711fcce76b393dbe5
Vishwanath <thevishwa01@gmail.com>
2026-05-08 15:24:35 +0530

    Split

---
+++ Assignments/src/StringClassMethods/SplitElements.java
@@ -0,0 +1,15 @@
+package StringClassMethods;
+
+public class SplitElements {
+public static void main(String[] args) {
+	
+	String a="a,b,c";
+	
+  for (int i = 0; i <a.length(); i++) {
+	  
+	 System.out.println(a.charAt(i));
+	
+}
+}
+}
+//How can you split the string "a,b,c" into individual elements?
