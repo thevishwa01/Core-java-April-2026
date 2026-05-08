13eb3e9fd1dbd1e9b772bbb980199bec6798d730
Vishwanath <thevishwa01@gmail.com>
2026-05-08 14:46:48 +0530

    Replace

---
+++ Assignments/src/StringClassMethods/ReplaceMethod.java
@@ -0,0 +1,10 @@
+package StringClassMethods;
+
+public class ReplaceMethod {
+public static void main(String[] args) {
+	
+	System.out.println("abc".replace('b', 'x'));
+	
+}
+}
+//What is the result of "abc".replace('b', 'x') ?
