b7b53de20e33c19053c869ceec055fbd81380577
Vishwanath <thevishwa01@gmail.com>
2026-05-08 13:25:16 +0530

    SubString between

---
+++ Assignments/src/StringClassMethods/SubStringbetween.java
@@ -0,0 +1,12 @@
+package StringClassMethods;
+
+public class SubStringbetween {
+	public static void main(String[] args) {
+		
+		String s="HelloWorld";
+		System.out.println(s.substring(5, 7));
+		
+	}
+
+}
+//Given String str = "Programming"; , what does str.substring(3, 6) return?
