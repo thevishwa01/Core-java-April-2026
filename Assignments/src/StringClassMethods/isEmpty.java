2ec7e3bbe98954ff4f72dd36f52c2c8e3df87361
Vishwanath <thevishwa01@gmail.com>
2026-05-08 14:53:44 +0530

    isEmpty

---
+++ Assignments/src/StringClassMethods/isEmpty.java
@@ -0,0 +1,10 @@
+package StringClassMethods;
+
+public class isEmpty {
+public static void main(String[] args) {
+	
+	System.out.println("".isEmpty());
+	System.out.println(" ".isEmpty());
+}
+}
+//What does "".isEmpty() return? What about " ".isEmpty() ?
