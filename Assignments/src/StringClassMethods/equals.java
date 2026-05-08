df3f51c55e8ab62dd72549b33dd3957ca6fa9679
Vishwanath <thevishwa01@gmail.com>
2026-05-08 15:34:14 +0530

    equals

---
+++ Assignments/src/StringClassMethods/equals.java
@@ -0,0 +1,9 @@
+package StringClassMethods;
+
+public class equals {
+public static void main(String[] args) {
+	
+	System.out.println("Hello".equals("Hello"));
+}
+}
+//What is the result of "Hello".equals(new String("Hello")) ? Why?
