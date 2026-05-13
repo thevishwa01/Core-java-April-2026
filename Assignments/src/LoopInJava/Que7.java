b204029dc2f35d5f382d8d981e5684710a3de357
Vishwanath <thevishwa01@gmail.com>
2026-05-13 14:29:32 +0530

    Divisible by 3 using loop

---
+++ Assignments/src/LoopInJava/Que7.java
@@ -0,0 +1,15 @@
+package LoopInJava;
+
+public class Que7 {
+
+	public static void main(String[] args) {
+		
+		
+		for (int i = 1; i <=30; i+=3) {
+			System.out.println(i);
+			
+		}
+		
+		
+	}
+}
