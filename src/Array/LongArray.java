e8884488ea525224c17e3abb9b695051f99d294c
Vishwanath <thevishwa01@gmail.com>
2026-05-13 12:30:18 +0530

    Long using Array

---
+++ src/Array/LongArray.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class LongArray {
+	public static void main(String[] args) {
+
+		long[] PhoneNum = { 9376328524l, 9027389274l, 9475667394l, 8466354792l, 7465829349l };
+
+		for (int i = 0; i < PhoneNum.length; i++) {
+
+			System.out.println(PhoneNum[i]);
+		}
+	}
+}
