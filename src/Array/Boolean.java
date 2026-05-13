e91e1adcb04241c3d4ce42375088955377d5659e
Vishwanath <thevishwa01@gmail.com>
2026-05-13 12:22:57 +0530

    Boolean Finding Percentage

---
+++ src/Array/Boolean.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class Boolean {
+	public static void main(String[] args) {
+
+		double[] Percentage = { 77.40, 94.34, 69.85, 83.75, 79.05 };
+
+		for (int i = 0; i < Percentage.length; i++) {
+
+			if (Percentage[i] >= 75) {
+
+				System.out.println(Percentage[i]);
+			}
+
+		}
+
+	}
+}
