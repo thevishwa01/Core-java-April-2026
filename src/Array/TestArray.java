9f76867072cf89e19755c59788ed1a27877a5a55
Vishwanath <thevishwa01@gmail.com>
2026-05-13 11:12:24 +0530

    Array

---
+++ src/Array/TestArray.java
@@ -0,0 +1,24 @@
+package Array;
+
+public class TestArray {
+	public static void main(String[] args) {
+		
+		
+		int [] Age= {10,20,30,40,40,60,70};
+		
+		System.out.println(Age[0]);
+		System.out.println(Age[1]);
+		System.out.println(Age[2]);
+		
+		System.out.println("====================");
+		
+		
+		
+		for (int i = 0; i <Age.length; i++) {
+		         
+			System.out.println(Age[i]);
+			
+		}
+	}
+
+}
