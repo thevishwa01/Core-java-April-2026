a93f4e2b23491bdcb4ac4ffb8fd642af63b68800
Vishwanath <thevishwa01@gmail.com>
2026-05-14 15:27:58 +0530

    Movie Ticket Booking Using Fixed Length of Array

---
+++ src/Array/MovieTicketBooking.java
@@ -0,0 +1,22 @@
+package Array;
+
+public class MovieTicketBooking {
+	public static void main(String[] args) {
+
+		
+		int[] SeatNo=new int[4];
+		
+		SeatNo[0]=20;
+		SeatNo[1]=21;
+		SeatNo[2]=22;
+		SeatNo[3]=23;
+		
+		for(int i=0;i<SeatNo.length;i++) {
+			System.out.println("Seat no is "+SeatNo[i]);
+			
+		}
+		
+		
+	}
+
+}
