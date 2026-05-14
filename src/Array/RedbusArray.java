81e039c9edb837aae8bcadbe9b62636e7bb53220
Vishwanath <thevishwa01@gmail.com>
2026-05-14 13:39:35 +0530

    Fixed Size of Array

---
+++ src/Array/RedbusArray.java
@@ -0,0 +1,26 @@
+package Array;
+
+public class RedbusArray {
+public static void main (String[] args) {
+	
+	
+	int[] redbus= new int[4];
+	
+	redbus[0]=9;
+	redbus[1]=10;
+	redbus[2]=11;
+	redbus[3]=12;
+	
+	
+	for(int i=0; i<redbus.length;i++) {
+	
+		  System.out.println("Selected seat no is :"+redbus[i]);  
+	  
+		
+		
+	}
+	
+	
+	
+}
+}
