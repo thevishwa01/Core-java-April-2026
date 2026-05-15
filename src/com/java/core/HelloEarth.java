5271dad2a5014247dcf677d9f96ad6386a6e7b19
Vishwanath <thevishwa01@gmail.com>
2026-05-15 11:26:20 +0530

    Revision

---
+++ src/com/java/core/HelloEarth.java
@@ -0,0 +1,10 @@
+package com.java.core;
+
+public class HelloEarth {
+public static void main(String[] args) {
+	
+	String a="Hello Earth";
+	System.out.println(a);
+	
+}
+}
---
+++ src/com/java/core/Operators2.java
@@ -0,0 +1,17 @@
+package com.java.core;
+
+public class Operators2 {
+public static void main(String[] args) {
+	
+	int a=20;
+	int b=20;
+	
+	int c=a+b;
+	int d=a/b;
+	int e=a*b;
+	
+	System.out.println("Addition is "+c+",  Division is "+d+",   Multiplication is "+e);
+	
+	
+}
+}
