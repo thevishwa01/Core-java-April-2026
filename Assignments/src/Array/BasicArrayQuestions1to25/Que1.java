8fe13e452ac677ead8ebb71ca5cb1b82de3a0770
Vishwanath <thevishwa01@gmail.com>
2026-06-05 13:59:42 +0530

    Array que 1

---
+++ Assignments/src/Array/BasicArrayQuestions1to25/Que1.java
@@ -0,0 +1,17 @@
+package Array.BasicArrayQuestions1to25;
+
+public class Que1 {
+	public static void main(String[] args) {
+
+		int[] a= {1,2,3,4,5,6,7,8,9,0};
+
+		for(int i=0;i<a.length;i++) {
+			
+			
+			System.out.println(a[i]);
+			
+		}
+		
+	}
+
+}
