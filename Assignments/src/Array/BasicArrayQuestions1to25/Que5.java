6d11e19fe15856f40db400b651db42aecf74d37e
Vishwanath <thevishwa01@gmail.com>
2026-06-10 15:00:50 +0530

    Sum of All Elements

---
+++ Assignments/src/Array/BasicArrayQuestions1to25/Que5.java
@@ -0,0 +1,19 @@
+package Array.BasicArrayQuestions1to25;
+
+public class Que5 {
+	public static void main(String[] args) {
+		
+		
+		int [] a= {1,2,3,4,5,6,7,8,9,10};
+		
+		int sum=0;
+		
+		
+		for (int i = 0; i < a.length; i++) {
+
+			sum+=a[i];
+		}
+		System.out.println("Sum of All Array : "+sum);
+	}
+
+}
