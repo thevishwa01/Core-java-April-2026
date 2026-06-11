f533f04f1c949ce18bdbedf62f820efe7ff8b010
Vishwanath <thevishwa01@gmail.com>
2026-06-11 12:43:17 +0530

    Element of one arry to another

---
+++ Assignments/src/Array/BasicArrayQuestions1to25/QUE12.java
@@ -0,0 +1,16 @@
+package Array.BasicArrayQuestions1to25;
+
+public class QUE12 {
+	public static void main(String[] args) {
+		int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		int[] b = new int[a.length];
+
+		for (int i = 0; i < a.length; i++) {
+
+			b[i]=a[i];
+
+			System.out.println(b[i]);
+		}
+	}
+
+}
