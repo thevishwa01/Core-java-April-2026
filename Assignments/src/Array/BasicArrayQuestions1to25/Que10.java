7a47a9509d151ea02466ddd3d89cd426f405b6c8
Vishwanath <thevishwa01@gmail.com>
2026-06-11 11:55:38 +0530

    Element at Even Indice

---
+++ Assignments/src/Array/BasicArrayQuestions1to25/Que10.java
@@ -0,0 +1,17 @@
+package Array.BasicArrayQuestions1to25;
+
+public class Que10 {
+	public static void main(String[] args) {
+		
+		int [] a= {10,22,34,53,28,94,72,96,71,67,893,2,95};
+		
+		for (int i = 0; i < a.length; i+=2) {
+			
+			System.out.println(a[i]);
+			
+		}
+		
+		
+	}
+
+}
