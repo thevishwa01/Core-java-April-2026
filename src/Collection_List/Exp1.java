14a22fc1efea21eae8493c6d314ad6fa81ed3147
Vishwanath <thevishwa01@gmail.com>
2026-06-15 13:28:21 +0530

    Collection List

---
+++ src/Collection_List/Exp1.java
@@ -0,0 +1,39 @@
+package Collection_List;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Exp1 {
+
+	public static void main(String[] args) {
+
+		List<String> l = new ArrayList<String>();
+
+		l.add("Apple");
+		l.add("Banana");
+		l.add("Grape");
+
+		System.out.println(l);
+		String result = "[" + String.join(",", l) + "]";
+		for (String string : l) {
+
+			System.out.println(string);
+
+		}
+
+		System.out.println(result);
+
+		System.out.println("======================");
+
+		List<String> la = new LinkedList<>();
+
+		la.add("Ankit");
+		la.add("Abhay");
+		la.add("Aditya");
+
+		System.out.println(la);
+
+	}
+
+}
