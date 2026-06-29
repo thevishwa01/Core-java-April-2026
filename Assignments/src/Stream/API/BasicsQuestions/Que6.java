1a7725e37b94f6ab96c9ffc4005b2849b592aeb4
Vishwanath <thevishwa01@gmail.com>
2026-06-29 15:21:45 +0530

    Assigment Stream API

---
+++ Assignments/src/Stream/API/BasicsQuestions/Que6.java
@@ -0,0 +1,16 @@
+package Stream.API.BasicsQuestions;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Que6 {
+	public static void main(String[] args) {
+
+		List<String> l = new LinkedList<>(
+				Arrays.asList("Vishwa", " ", "Prashant", "Anmol", " ", "Rohit", "Aditya", ""));
+
+		l.stream().filter(n -> n != null && !n.trim().isEmpty()).forEach(System.out::println);
+	}
+
+}
