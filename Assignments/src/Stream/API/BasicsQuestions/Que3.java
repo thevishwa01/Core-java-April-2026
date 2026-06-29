1938ea0245b4fa86d6a7a43b6c19ee9b45b1deaf
Vishwanath <thevishwa01@gmail.com>
2026-06-29 13:04:16 +0530

    Assignment Stream API

---
+++ Assignments/src/Stream/API/BasicsQuestions/Que3.java
@@ -0,0 +1,20 @@
+package Stream.API.BasicsQuestions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Que3 {
+	public static void main(String[] args) {
+
+		List<String> l = new ArrayList<String>(Arrays.asList("animal", "bird", "fish", "reptile"));
+
+		for (String s : l) {
+			System.out.println(s.toUpperCase());
+		}
+
+		l.stream().map(n -> n.toUpperCase()).forEach(System.out::println);
+		;
+	}
+
+}
