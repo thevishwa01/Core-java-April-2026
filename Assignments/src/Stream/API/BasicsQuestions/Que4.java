87479cc01ed864f6088b66c1107de50270d4096e
Vishwanath <thevishwa01@gmail.com>
2026-06-29 13:18:48 +0530

    Assignment Stream API

---
+++ Assignments/src/Stream/API/BasicsQuestions/Que4.java
@@ -0,0 +1,24 @@
+package Stream.API.BasicsQuestions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Que4 {
+	public static void main(String[] args) {
+
+		List<String> l = new ArrayList<String>(Arrays.asList("Vishwanat", "Ankit", "Makrand", "Ankit", "Kshitij",
+				"Abhay", "Dipansh", "Akash", "Anmol"));
+
+		l.stream().filter(n -> n.startsWith("A")).forEach(System.out::println);
+
+		for (String s : l) {
+			if (s.startsWith("A")) {
+				System.out.println(s);
+
+			}
+
+		}
+
+	}
+}
