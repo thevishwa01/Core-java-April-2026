1430031653d28468672c42a9993a3cdb7bbd8123
Vishwanath <thevishwa01@gmail.com>
2026-06-30 15:01:27 +0530

    Count How Many numbers are Even

---
+++ Assignments/src/Basic/Collection/Assignment/Set/Que4.java
@@ -0,0 +1,20 @@
+package Basic.Collection.Assignment.Set;
+
+import java.util.Set;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Que4 {
+	public static void main(String[] args) {
+
+		Set<Integer> l = new HashSet<>(
+				Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20));
+		int count = 0;
+		for (Integer i : l) {
+			if (i % 2 == 0) {
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+}
