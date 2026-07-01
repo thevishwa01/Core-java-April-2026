47f5e964e9ca1d2d2e35c9749c19e921bcc6b635
Vishwanath <thevishwa01@gmail.com>
2026-07-01 18:53:25 +0530

    print the string with the maximum number of vowels

--- Assignments/src/Basic/Collection/Assignment/Set/Que11.java
+++ Assignments/src/Basic/Collection/Assignment/Set/Que11.java
@@ -11,25 +11,24 @@ public class Que11 {
 
 		Set<String> z = new HashSet<String>(Arrays.asList(l.split(" ")));
 
-	
 		String a = "aeiou";
 		String max = "";
-		int maxcount=-1;
-		
+		int maxcount = -1;
+
 		for (String s : z) {
-			int count=0;
+			int count = 0;
 			for (char c : s.toLowerCase().toCharArray()) {
 				if (a.indexOf(c) != -1) {
 					count++;
 				}
 
 			}
-			if (count>maxcount) {
-				maxcount=count;
-				max=s;
-				
+			if (count > maxcount) {
+				maxcount = count;
+				max = s;
+
 			}
 		}
-		System.out.println("The word that contains maximum Vowels : "+max);
+		System.out.println("The word that contains maximum Vowels : " + max);
 	}
 }
